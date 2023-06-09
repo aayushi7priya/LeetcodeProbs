38132afde61f837677c9f0881496ff8a3e6f2eb7
Aayushi Priya <86151915+redheadpanda@users.noreply.github.com>
2023-06-09 19:11:24 +0530

    Time: 34 ms (16.15%), Space: 15.1 MB (13.97%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,34 +1,48 @@
 class Solution {
 public:
-
-void dfs(int s, int n, vector<vector<int>>& isConnected, vector<int> &visited)
-{
-    visited[s] = 1;
-
-    for(int i=0;i<n;i++)
+    vector<bool> vis;
+    vector<vector<int>>adj;
+    void dfs(int node)
     {
-        if(isConnected[s][i]==1)
+        vis[node] = true;
+        //ls.push_back(node);
+        
+        for(auto i: adj[node])
         {
-            if(!visited[i])
-                dfs(i, n, isConnected, visited);
-            else
-                continue;
-        }
-    }
-}
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size(); //total number of cities
-        vector<int>visited(n,0); 
-        int count =0;
-
-        for(int i=0;i<n;i++)
-        {
-            if(!visited[i])
+            if(!vis[i])
             {
-                count++;
-               dfs(i, n, isConnected, visited); 
+                dfs(i);
             }
         }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        //adjacency list
+       int n = isConnected.size();
+        vis.resize(n, false);
+        adj.resize(n);
+        for(int i = 0;i<n;i++)
+        {
+            for(int j = 0;j<n;j++)
+            {
+                if(isConnected[i][j]==1 && i!=j)
+                {
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+            }
+        }
+        
+        int count = 0;
+        for(int i = 0; i<n;i++)
+        {
+            if(!vis[i])
+            {
+                count++;
+                dfs(i);
+            }
+            
+        }
         return count;
+        
     }
 };
