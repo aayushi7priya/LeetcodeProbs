63e79d66b64319e6b47eab02fc91a503613ee88f
Aayushi Priya <86151915+redheadpanda@users.noreply.github.com>
2023-07-01 21:54:11 +0530

    Time: 27 ms (71.21%), Space: 25.2 MB (11.95%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,42 +1,27 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int product = 1,cnt=0;
-        for(int i =0;i<nums.size();i++)
+        //imp: don't use division
+        int n = nums.size();
+        vector<int>pref(n,1);
+        vector<int>suff(n,1);
+        pref[0] = nums[0];
+        suff[n-1] = nums[n-1];
+        for(int i =1;i<nums.size();i++)
         {
-            if(nums[i]!=0)
-            {
-                product*=nums[i];
-            }
-            else if(nums[i]==0)
-            {
-                cnt++;
-            }
+            pref[i] = pref[i-1]*nums[i];  
+        }
+        for(int i =n-2;i>=0;i--)
+        {
+            suff[i] = suff[i+1]*nums[i];
         }
         
-        vector<int>ans(nums.size());
-        if(cnt==0)
+        vector<int>ans(n);
+        ans[0] = suff[1];
+        ans[n-1] = pref[n-2];
+        for(int i =1;i<n-1;i++)
         {
-            for(int i =0;i<nums.size();i++)
-            {
-                ans[i]= product/nums[i];
-            }
-        }
-        else if(cnt==1)
-        {
-            for(int i=0;i<nums.size();i++)
-            {
-                if(nums[i]!=0)
-                {
-                    ans[i]=0;
-                }
-                else ans[i] = product;
-            }
-        }
-        else 
-        {
-            for(int i =0;i<nums.size();i++)
-                ans[i] = 0;
+            ans[i] = suff[i+1]*pref[i-1];
         }
         return ans;
     }
