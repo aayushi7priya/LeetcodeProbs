1208bb6bafe8a9f864b7c76f3c624813104ebd12
Aayushi Priya <86151915+redheadpanda@users.noreply.github.com>
2023-07-07 16:20:01 +0530

    Time: 9 ms (79.58%), Space: 11.7 MB (19.07%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -1,18 +1,25 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution(object):
-    def deleteDuplicates(self, head):
-        cur = head
-        
-        #two loops
-        #first one to keep track of value of cur pointer
-        # inside loop to delete all same values with cur
-        while cur:
-            while cur.next and cur.next.val==cur.val:
-                cur.next = cur.next.next
-            cur = cur.next
-        return head
-        
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* curr = head;
+        while(curr!=nullptr)
+        {
+            while(curr->next!=nullptr && curr->next->val == curr ->val)
+            {
+                curr->next = curr->next->next;
+            }
+            curr = curr->next;
+        }
+        return head;
+    }
+};
